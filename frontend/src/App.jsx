54d8e5aeb699d897e7d4fb1f318d1cd545b8eb72
Sakshyam-RT14 <pokhrelsakshyam8@gmail.com>
2026-03-14 21:27:30 +0545

    point frontend to Render backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
-const BASE_URL = "http://127.0.0.1:8000/api/grocery";
+const BASE_URL = "https://drf-crud-fq6y.onrender.com/api/grocery";
 
 function App() {
   const [items, setItems] = useState([]);
